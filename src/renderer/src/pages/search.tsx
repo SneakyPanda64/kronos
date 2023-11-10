f66dcf3019af41e071e3ea0ef209d4843cac02fb
SneakyPanda <34578192+SneakyPanda64@users.noreply.github.com>
2023-11-10 17:51:26 +0000

    fix: search button bug

--- src/renderer/src/pages/search.tsx
+++ src/renderer/src/pages/search.tsx
@@ -10,8 +10,8 @@ export default function SearchPage() {
     })
   }
   return (
-    <div className="flex w-screen h-screen">
-      <div className="mx-auto pt-60 text-center justify-center w-1/2">
+    <div className="flex w-screen h-screen select-none cursor-text">
+      <div className="mx-auto pt-40 text-center justify-center w-3/4 md:w-1/2 lg:w-1/3">
         <h1 className="text-6xl pb-12">Amenoi</h1>
         <div
           className="w-full h-12 px-4 bg-s-blue bg-opacity-20 rounded-xl flex"
